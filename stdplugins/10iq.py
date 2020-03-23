db2f45041c1bd59d34d3455a623e9ca7f1ab2782
Erdem Özdemir <erdemozdemir@mail.com>
2020-03-23 17:09:33 +0300

    Re-add dbplugins and sql helpers

--- stdplugins/10iq.py
+++ stdplugins/10iq.py
@@ -25,5 +25,5 @@ from telethon import events
 from uniborg.util import admin_cmd
 
 @borg.on(admin_cmd(pattern="10iq ?(.*)", outgoing=True))
-async def 10iq(e):
+async def iqless(e):
     await e.edit("DÜÜÜT DÜÜÜTT AÇ YOLU AÇÇ HADİ ASLAN PARÇASI YOLU AÇ HADİ BAK ENGELLİ BEKLİYO BURDA HADİ DÜÜÜTTT ♿️ BAK SİNİRLENDİ ARKADAŞ HADİ YOLU AÇ HADİİ DÜÜÜT DÜÜTT BİİİPP HADİ BE HIZLI OLL DÜÜÜTT BİİİPPP ♿️♿️ BAK HIZLANDI ENGELLİ KARDEŞİMİZ SERİ KÖZ GETİR SERİ DÜÜÜTT DÜÜÜT DÜÜÜÜTTTTT BİİİİPPP BİİİİİPPP DÜÜÜTTT ♿️♿️♿️♿️ BAK ARTIYO SAYILARI AÇTIN MI YOLU AÇMADIN PÜÜÜÜ REZİİİLL DÜÜÜÜTTT ♿️♿️♿️♿️♿️♿️ BAK KALABALIKLASTI BAK DELI GELIYOR DELIRDI DELI AC YOLU DUTDUTDURURURUDUTTT♿️♿️♿️♿️♿️♿️♿️♿️♿️♿️♿️♿️♿️♿️KAFAYI YEDI BUNLAR AC LAAAAN YOLU")
