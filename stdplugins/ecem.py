63789b23115a6fc8138f3eee2a2d431a250ce12e
Erdem Özdemir <39374133+erdemozdemir98@users.noreply.github.com>
2020-03-24 23:21:36 +0300

    Ecem.py eklendi. Büyük ihtimalle repodaki son commit.

---
+++ stdplugins/ecem.py
@@ -0,0 +1,331 @@
+# Ecem Quotes
+# Modül erdemozdemir98'den, özlü sözler ciyandan
+# CİYAN SİKİİİİİMMMMMMMMMMMMMMMMMMMMMMMM!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+from telethon import events
+
+import asyncio
+
+import os
+
+import sys
+
+import random
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.ecem", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    await event.edit("Ecem'in saçlarıyla oynanıyor...")
+
+    await asyncio.sleep(2)
+
+    x=(random.randrange(1,100))
+
+    if x==1:
+
+        await event.edit("Erkek arkadaşlarımın arkadaşlarının adlarını bilmem normal bir şey.")
+
+    if x==2:
+
+    	await event.edit("Şu an acele işim var, daha sonra çekilsek olmaz mı? Zaten yine geleceğim.")
+
+    if x==3:
+    	await event.edit("Yok, kızmadım.")
+
+    if x==4:
+    	await event.edit("Bunun farkına vardıysan benim için artık problem yok.")
+
+    if x==5:
+    	await event.edit("Bundan sonra artık hayatında Ecem diye biri yok!")
+
+    if x==6:
+    	await event.edit("Yağmurlu havada sweatini veren arkadaşlarınız olsun. :p")
+
+    if x==7:
+    	await event.edit("Yalnız çiçek açar mısın? Yalnız, çiçek.")
+
+    if x==8:
+    	await event.edit("Affetmiyorum, annemi çağıracağım. O gereğini yapacak.")
+
+    if x==9:
+    	await event.edit("Saçımla oynama.")
+
+    if x==10:
+    	await event.edit("Saçımla oynamamanı söylediğim halde oynuyorsun. Arkamdan git lütfen.")
+
+    if x==11:
+    	await event.edit("Ben gelmiyorum bedene falan, kenarda ölmeyi bekleyeceğim.")
+
+    if x==12:
+    	await event.edit("Düşman değiliz, sadece onun olduğu yerde rahat edemem.")
+
+    if x==13:
+    	await event.edit("Git arkamdan, istemiyorum seni!")
+
+    if x==14:
+    	await event.edit("Murat Boz şarkıları beni hiç eğlendirmiyor.")
+
+    if x==15:
+    	await event.edit("Tövbe tövbe kapatın şunu çarpılacağız.")
+
+    if x==16:
+    	await event.edit("Üç kızım olsun istiyorum.")
+
+    if x==17:
+    	await event.edit("Eğer onunla evlenirsem nikah şahidim sen ol.")
+
+    if x==18:
+    	await event.edit("Ehehehe çok komik, hadi defolun gidin.")
+
+    if x==19:
+    	await event.edit("Gidip onunla konuşsana?")
+
+    if x==20:
+    	await event.edit("Ne oldu sana? Anlatmak ister misin?")
+
+    if x==21:
+    	await event.edit("Lütfen anlat işte. Çok mu özel ki?")
+
+    if x==22:
+    	await event.edit("Hadi gel erkeklerin yanına geçelim.")
+
+    if x==23:
+    	await event.edit("İyi misin?")
+
+    if x==24:
+    	await event.edit("Teşekkür ederim :)))")
+
+    if x==25:
+    	await event.edit("Daha sonra yine geleceğim, o zaman çekiliriz, olmaz mı?")
+
+    if x==26:
+    	await event.edit("Grubu dağıtıyoruz gençler.")
+
+    if x==27:
+    	await event.edit("Grubu dağıtıyoruz gençler??? Ne ara dedim onu ya ben?")
+
+    if x==28:
+    	await event.edit("Allah belanı vermesin.")
+
+    if x==29:
+    	await event.edit("Hadi yönetici sensin, dağıt grubu.")
+
+    if x==30:
+    	await event.edit("İyi akşamlar.")
+
+    if x==31:
+    	await event.edit("Her şeyi ben söylüyorum, biraz da siz söyleyin.")
+
+    if x==32:
+    	await event.edit("Ben onu esprisine demiştim diyette değilim.")
+
+    if x==33:
+    	await event.edit("Sizde var mı bilmiyorum sorim dedim birkaç kişiye daha sordum, çıktı alabilme şansın var mı?")
+
+    if x==34:
+    	await event.edit("Üçgende çıkmış sorular, 3. site.")
+
+    if x==35:
+    	await event.edit("Eğer olmadıysa gerek kalmadı.")
+
+    if x==36:
+    	await event.edit("Yok yani, ben buldum birinden.")
+
+    if x==37:
+    	await event.edit("Evet evet hallettim ben sağol.")
+
+    if x==38:
+    	await event.edit("Atim sen de kontrol et bir, o mu diye.")
+
+    if x==39:
+    	await event.edit("Neyse sen çıkarttığını da getirirsin olmadı.")
+
+    if x==40:
+    	await event.edit("Kim ne dedi sana?")
+
+    if x==41:
+    	await event.edit("Onlar benim kardeşim gibi olduğu için sahipleniyorlar.")
+
+    if x==42:
+    	await event.edit("Kötü bir durum yok.")
+
+    if x==43:
+    	await event.edit("Bir anda yapınca rahatsız oldum ama sonra sana zaten sorun yok dedim.")
+
+    if x==44:
+    	await event.edit("Onlar da biliyor, ondan tepki vermiştir.")
+
+    if x==45:
+    	await event.edit("Sorun yok yani.")
+
+    if x==46:
+    	await event.edit("Rica ederim.")
+
+    if x==47:
+    	await event.edit("Teşekkür ederim kusura bakma telefonum bozuktu yeni görebildim :)")
+
+    if x==48:
+    	await event.edit("Evet. Yalan borcum mu var size? Evet, yalan borcum mu var? Yalan borcum mu var?")
+
+    if x==49:
+    	await event.edit("Ne malsın ya.")
+
+    if x==50:
+    	await event.edit("Testleri bitirmeyin Allah aşkına.")
+
+    if x==51:
+    	await event.edit("Sen mesajı silsen de bildirimde gözüküyor.")
+
+    if x==52:
+    	await event.edit("O ne demişti ki?")
+
+    if x==53:
+    	await event.edit("Ben birine bir şey yazmştım, sonra sildim. Ne yazdın dedi, söylemedim. Sonra bildirimden açıp bana yazdı, yazdığım şeyi.")
+
+    if x==54:
+    	await event.edit("Mzkddkslslldldldldld")
+
+    if x==55:
+    	await event.edit("Toplu olarak yapıyoruz değil mi, hiç hoş olmayan şeyler çıkmasın sonra.")
+
+    if x==56:
+    	await event.edit("Lan sen konuşma.")
+
+    if x==57:
+    	await event.edit("Abi yalan değil ki, yetişmiyor.")
+
+    if x==58:
+    	await event.edit("Ya hep mi korktuğum başıma gelir ya?")
+
+    if x==59:
+    	await event.edit("Bende şans olsa zaten.")
+
+    if x==60:
+    	await event.edit("Ben oyuna falan katılamam.")
+
+    if x==61:
+    	await event.edit("Tövbe, hep olmaması gereken sınıfları söylüyorsun.")
+
+    if x==62:
+    	await event.edit("Neyse ben konuşmuyorum sizinle zaten.")
+
+    if x==63:
+    	await event.edit("Ne uzattınız ya, değişmiş işte.")
+
+    if x==64:
+    	await event.edit("Sen sus, seninle konuşan yok.")
+
+    if x==65:
+    	await event.edit("Çok özledim :( ♥️")
+
+    if x==66:
+    	await event.edit("Saygı, minnet ve özlemle.")
+
+    if x==67:
+    	await event.edit("Canım, dayım, her şeyim yolun açık olsun askerim ♥️")
+
+    if x==68:
+    	await event.edit("#NoFilter")
+
+    if x==69:
+    	await event.edit("Yorgunum ve ağrılar")
+
+    if x==70:
+    	await event.edit("Sıkıldım ya, konuşun. Söz valla terslemeyeceğim.")
+
+    if x==71:
+    	await event.edit("Çok kaptırmış kendini o.")
+
+    if x==72:
+    	await event.edit("Okul mal o zaman, o kadar yanlışla on birinci olduysam.")
+
+    if x==73:
+    	await event.edit("Kime diyorsunuz, ben bir şey anlamadım.")
+
+    if x==74:
+    	await event.edit("Sen hani hastaydın Lan")
+
+    if x==75:
+    	await event.edit("Sen ne diyon ya sksödödödöföfçgçgçhçhş")
+
+    if x==76:
+    	await event.edit("Hiç bu kadar güldüğümü hatırlamıyorum.")
+
+    if x==77:
+    	await event.edit("Kaç dakika uğraştın, gerizekalı ya.")
+
+    if x==78:
+    	await event.edit("Lan sus sende sabahtan beri Ecem Ecem dmdkdmdmdmföfögö")
+
+    if x==79:
+    	await event.edit("Evde gerizekalı gibi gülüyorum susun artık.")
+
+    if x==80:
+    	await event.edit("Tövbe yarabbim")
+
+    if x==81:
+    	await event.edit("Üstüme gitmeyin şu saatlerde benim.")
+
+    if x==82:
+    	await event.edit("Sakn ol raki")
+
+    if x==83:
+    	await event.edit("5 saattir anlamaya çalışıyor.")
+
+    if x==84:
+    	await event.edit("Yeter, gidin yatın.")
+
+    if x==85:
+    	await event.edit("Sus lan mal")
+
+    if x==86:
+    	await event.edit("Gahahaha diye gülenin dediğine bak.")
+
+    if x==87:
+    	await event.edit("Yarın hiç hoş olmayacaksın.")
+
+    if x==88:
+    	await event.edit("Sinirliyim zaten, uğraşmayın benimle. Sjdkdkkdmfgm")
+
+    if x==89:
+    	await event.edit("Bu arada yüzümü telefon gözükmesin diye öyle tutmuyordum sebebini bilmiyorum djjd")
+
+    if x==90:
+    	await event.edit("Doğum günü kızıyım ben şşş. Nasıl bir değişiklik bekliyordunuz dkdkdk")
+
+    if x==91:
+    	await event.edit("İnternetten baksana")
+
+    if x==92:
+    	await event.edit("Müzik dersi hangi gün? Hocaya söz vermiştim.")
+
+    if x==93:
+    	await event.edit("Hem arkamdan kaşar diyorsun, hem de kardeşim diyorsun.")
+
+    if x==94:
+    	await event.edit("Böyle böyle yürüyor ya ahahahahahahaha")
+
+    if x==95:
+    	await event.edit("Her an'ımda")
+
+    if x==96:
+    	await event.edit("bff bff bff forever bff")
+
+    if x==97:
+    	await event.edit("Yine, yeni, yeniden ♥️")
+
+    if x==98:
+    	await event.edit("Buraya sığdıramayacağım milyon tane iyi anımız var, hep de olsun.")
+
+    if x==99:
+    	await event.edit("İyi ki doğdun kız kardeşim, seni çok seviyorum ♥️")
+
+    if x==100:
+    	await event.edit("Çocukluk aşkımsın")
