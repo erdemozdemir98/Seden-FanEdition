10c4a77bb1e4f28221ee2d89587854275163e225
Erdem Özdemir <39374133+erdemozdemir98@users.noreply.github.com>
2020-03-24 18:44:59 +0300

    Update android.py

--- stdplugins/android.py
+++ stdplugins/android.py
@@ -105,56 +105,54 @@ async def codename_info(request):
         reply = f"`Couldn't find {device} codename!`\n"
     await request.edit(reply)
 
-    @borg.on(admin_cmd(pattern='specs(?: |)([\S]*)(?: |)([\s\S]*)'))
-    async def device_specifications(request):
-    """ search for android device specs """
-    textx = await request.get_reply_message()
-    brand = request.pattern_match.group(1).lower()
-    device = request.pattern_match.group(2).lower()
-    if brand and device:
-        pass
-    elif textx:
-        brand = textx.text.split(' ')[0]
-        device = ' '.join(textx.text.split(' ')[1:])
-    else:
-        await request.edit("`Usage: .specs <brand> <device>`")
-        return
-    all_brands = BeautifulSoup(
-        get('https://www.devicespecifications.com/en/brand-more').content,
-        'lxml').find('div', {
-            'class': 'brand-listing-container-news'
-        }).findAll('a')
-    brand_page_url = None
-    try:
-        brand_page_url = [
-            i['href'] for i in all_brands if brand == i.text.strip().lower()
-        ][0]
-    except IndexError:
-        await request.edit(f'`{brand} is unknown brand!`')
-    devices = BeautifulSoup(get(brand_page_url).content, 'lxml') \
-        .findAll('div', {'class': 'model-listing-container-80'})
-    device_page_url = None
-    try:
-        device_page_url = [
-            i.a['href']
-            for i in BeautifulSoup(str(devices), 'lxml').findAll('h3')
-            if device in i.text.strip().lower()
-        ]
-    except IndexError:
-        await request.edit(f"`can't find {device}!`")
-    if len(device_page_url) > 2:
-        device_page_url = device_page_url[:2]
-    reply = ''
-    for url in device_page_url:
-        info = BeautifulSoup(get(url).content, 'lxml')
-        reply = '\n' + info.title.text.split('-')[0].strip() + '\n'
-        info = info.find('div', {'id': 'model-brief-specifications'})
-        specifications = re.findall(r'<b>.*?<br/>', str(info))
-        for item in specifications:
-            title = re.findall(r'<b>(.*?)</b>', item)[0].strip()
-            data = re.findall(r'</b>: (.*?)<br/>', item)[0]\
-                .replace('<b>', '').replace('</b>', '').strip()
-            reply += f'**{title}**: {data}\n'
-    await request.edit(reply)
-
-    
+@borg.on(admin_cmd(pattern='specs(?: |)([\S]*)(?: |)([\s\S]*)'))
+async def device_specifications(request):
+  """ search for android device specs """
+  textx = await request.get_reply_message()
+  brand = request.pattern_match.group(1).lower()
+  device = request.pattern_match.group(2).lower()
+  if brand and device:
+      pass
+  elif textx:
+      brand = textx.text.split(' ')[0]
+      device = ' '.join(textx.text.split(' ')[1:])
+  else:
+      await request.edit("`Usage: .specs <brand> <device>`")
+      return
+  all_brands = BeautifulSoup(
+      get('https://www.devicespecifications.com/en/brand-more').content,
+      'lxml').find('div', {
+          'class': 'brand-listing-container-news'
+      }).findAll('a')
+  brand_page_url = None
+  try:
+      brand_page_url = [
+          i['href'] for i in all_brands if brand == i.text.strip().lower()
+      ][0]
+  except IndexError:
+      await request.edit(f'`{brand} is unknown brand!`')
+  devices = BeautifulSoup(get(brand_page_url).content, 'lxml') \
+      .findAll('div', {'class': 'model-listing-container-80'})
+  device_page_url = None
+  try:
+      device_page_url = [
+          i.a['href']
+          for i in BeautifulSoup(str(devices), 'lxml').findAll('h3')
+          if device in i.text.strip().lower()
+      ]
+  except IndexError:
+      await request.edit(f"`can't find {device}!`")
+  if len(device_page_url) > 2:
+      device_page_url = device_page_url[:2]
+  reply = ''
+  for url in device_page_url:
+      info = BeautifulSoup(get(url).content, 'lxml')
+      reply = '\n' + info.title.text.split('-')[0].strip() + '\n'
+      info = info.find('div', {'id': 'model-brief-specifications'})
+      specifications = re.findall(r'<b>.*?<br/>', str(info))
+      for item in specifications:
+          title = re.findall(r'<b>(.*?)</b>', item)[0].strip()
+          data = re.findall(r'</b>: (.*?)<br/>', item)[0]\
+              .replace('<b>', '').replace('</b>', '').strip()
+          reply += f'**{title}**: {data}\n'
+  await request.edit(reply)
